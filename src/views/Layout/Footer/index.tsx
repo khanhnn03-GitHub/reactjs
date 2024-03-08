eeabcc435435eb8cb919e58b1279d8e87c34be56
= <nguyenkhanhh.0205@gmail.com>
2024-03-08 18:40:10 +0700

    q

--- src/views/Layout/Footer/index.tsx
+++ src/views/Layout/Footer/index.tsx
@@ -8,26 +8,26 @@ const Footer = () => {
     <>
         <div className="footer">
             <div className="footer-content">
-                <div>
-                    <img className="anh1" src={anh1}></img>
+                <div className="footer-content11">
+                    {/*<img className="anh1" src={anh1}></img>*/}
                     <div className="footer-content1">
-                        <div className="footer-content2">
+                        <div className="footer-content2-1">
                             <div className="ft-contact">
-                                <h3>VIỆN NGHIÊN CỨU THẢO DƯỢC VIỆT NAM</h3>
+                                <h3 className="ft-nav-text">VIỆN NGHIÊN CỨU THẢO DƯỢC VIỆT NAM</h3>
                                 <div className="ft-contact-address">Địa chỉ: Số 54 ngõ 44 - Đường Đông Thắng - Phường Đông Ngạc - Quận Bắc Từ Liêm - Hà Nội</div>
                                 <div className="ft-contact-tel">Số điện thoại: 098.224.8922 - 024.62972174</div>
                                 <div className="ft-contact-email">Email: nhabvtv@gmail.com</div>
                             </div>
                         </div>
-                        <div className="footer-content2">
+                        <div className="footer-content2-2">
                             <div className="ft-nav">
-                                <h3>SẢN PHẨM</h3>
+                                <h3 className="ft-nav-text">SẢN PHẨM</h3>
                                 <div className="ft-nav1">Đông trùng hạ thảo tươi</div>
                             </div>
                         </div>
-                        <div className="footer-content2">
+                        <div className="footer-content2-3">
                             <div className="ft-nav">
-                                <h3>MAIN MENU</h3>
+                                <h3 className= "ft-nav-text">MAIN MENU</h3>
                                 <ul>
                                     <li>Trang chủ</li>
                                     <li>Giới thiệu</li>
@@ -40,9 +40,9 @@ const Footer = () => {
                                 </ul>
                             </div>
                         </div>
-                        <div className="footer-content2">
-                            <div>
-                                <h3>ĐĂNG KÝ NHẬN TIN</h3>
+                        <div className="footer-content2-4">
+                            <div className="footer-content2-4-1">
+                                <h3 className= "ft-nav-text">ĐĂNG KÝ NHẬN TIN</h3>
                                 <div className="ft-sub-desc">Hãy nhập địa chỉ email của bạn vào ô dưới đây để có thể nhận được tất cả các tin tức mới nhất của Viện bảo vệ thực vật về các sản phẩm mới, các chương trình khuyến mãi mới. Chúng tôi xin đảm bảo sẽ không gửi mail rác, mail spam tới bạn.</div>
                                 <div className="ft-sub-wrapper">
                                     <div className="ft-sub-wrapper1">
--- src/views/Layout/Header/index.tsx
+++ src/views/Layout/Header/index.tsx
@@ -1,6 +1,6 @@
 import { Layout } from "antd"
 import { IoIosCart } from "react-icons/io";
-
+import { RiArrowDropDownLine } from "react-icons/ri";
 import './style.scss';
 import React from "react";
 
@@ -12,12 +12,18 @@ const Header = () => {
             <div className="header1">
                 <a className="header-text">TRANG CHỦ</a>
                 <a className="header-text">GIỚI THIỆU</a>
-                <a className="header-text">SẢN PHẨM</a>
+                <a className="header-text">SẢN PHẨM
+                    <span > <RiArrowDropDownLine style={{ fontSize: '25px' } } /> </span>
+                </a>
                 <a className="header-text">TIN TỨC</a>
-                <a className="header-text">HÌNH ẢNH-VIDEO</a>
+                <a className="header-text">HÌNH ẢNH-VIDEO
+                    <span > <RiArrowDropDownLine style={{ fontSize: '25px' } } /> </span>
+                </a>
                 <a className="header-text">PHÂN TÍCH-KIẺM NGHIỆM</a>
                 <a className="header-text">DANH SÁCH ĐẠI LÝ</a>
-                <a className="header-text">LIÊN HỆ</a>
+                <a className="header-text">LIÊN HỆ
+                    <span > <RiArrowDropDownLine style={{ fontSize: '25px' } } /> </span>
+                </a>
                 <a className="header-text" > <IoIosCart /> </a>
             </div>
 
